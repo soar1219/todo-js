6f36a4013dc5b6b7c6e9db8510f1d374ca5326a0
soar1219 <tsubasa1219.info@gmail.com>
2023-09-10 06:43:15 +0900

    テキストボックスの値を取得して初期化する

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  /* テキストボックスの値を取得し、初期化する */
+  const inputText = document.getElementById("add_text").value;
+  document.getElementById("add_text").value = "";
+  alert(inputText);
+}
+
+document.getElementById("add_button")
+.addEventListener("click",() => onClickAdd());

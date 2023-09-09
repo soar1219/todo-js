92771b3424c0d2221979c477f45d6bff60386840
soar1219 <tsubasa1219.info@gmail.com>
2023-09-10 07:18:52 +0900

    未完了リストの追加

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,21 @@ const onClickAdd = () => {
   /* テキストボックスの値を取得し、初期化する */
   const inputText = document.getElementById("add_text").value;
   document.getElementById("add_text").value = "";
-  alert(inputText);
+
+  /* ulの生成 */
+  const ul = document.createElement("ul");
+  ul.className = "list_row"; /* クラスの追加 */
+
+  /* liの生成 */
+  const li = document.createElement("li");
+  li.innerText = inputText;　/* テキストの追加 */
+
+  /* ulタグの子要素に各要素を設定 */
+  ul.appendChild(li);
+
+  /* 未完了リストに追加 */
+  document.getElementById("incomplete_list").appendChild(ul);
+
 }
 
 document.getElementById("add_button")

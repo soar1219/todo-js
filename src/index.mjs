5cb12f1355f91fe54114ad8060899f02e43df6e4
soar1219 <tsubasa1219.info@gmail.com>
2023-09-13 13:05:43 +0900

    ボタンにタスク追加機能の実装

--- src/index.mjs
+++ src/index.mjs
@@ -13,8 +13,25 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;　/* テキストの追加 */
 
+  /* ボタン（完了）タグの生成 */
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  /* ボタン（削除）タグの生成 */
+  const deleateButton = document.createElement("button");
+  deleateButton.innerText = "削除";
+  deleateButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   /* ulタグの子要素に各要素を設定 */
   ul.appendChild(li);
+  ul.appendChild(completeButton);
+  ul.appendChild(deleateButton);
+
 
   /* 未完了リストに追加 */
   document.getElementById("incomplete_list").appendChild(ul);
